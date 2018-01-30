a44e69ad46a62e20351ac614ce744c10a9eb6dbf
Mario Souto <soutomarios@gmail.com>
2018-01-30 11:43:37 -0200

    comitinhos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormNovoCartaoComponent } from './components/form-novo-cartao/form-novo-cartao.component';
+import { MuralComponent } from './components/mural/mural.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { CartaoComponent } from './components/cartao/cartao.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormNovoCartaoComponent,
+    MuralComponent,
+    CabecalhoComponent,
+    CartaoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/cartao/cartao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cartao',
+  templateUrl: './cartao.component.html',
+  styleUrls: ['./cartao.component.css']
+})
+export class CartaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
